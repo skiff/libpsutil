566d011d56ef4586137457b63eacf6a9b4358d93
Alex <skiffaw@g.cofc.edu>
2021-07-01 18:51:00 -0400

    change the way symbols generate opd

--- libpsutil/symbol.hpp
+++ libpsutil/symbol.hpp
@@ -3,22 +3,18 @@
 
 namespace libpsutil
 {
+    namespace symbol_helper
+    {
+        void* get_symbol(unsigned int address);
+    }
+
     template<typename T> class symbol
     {
-    private:
-        static int opd_table[10][2];
-        static int opd_table_id;
-
     public:
         operator T* ()
         {
-            if (opd_table_id > 9) { opd_table_id = 0; }
-
-            opd_table[opd_table_id][0] = this->mp_object_;
-            opd_table[opd_table_id][1] = memory::get_game_toc();
-
             T* type = *reinterpret_cast<T*>(this->mp_object_);
-            return (decltype(type))opd_table[opd_table_id++];
+            return (decltype(type))symbol_helper::get_symbol(this->mp_object_);
         }
 
         T* get()
@@ -28,7 +24,4 @@ namespace libpsutil
 
         int mp_object_;
     };
-
-    template <typename T> int symbol<T>::opd_table[10][2];
-    template <typename T> int symbol<T>::opd_table_id;
 }
