a9dd34caf08c7cd74f98bed38d0d843b98096722
Alex <skiffaw@g.cofc.edu>
2021-08-15 13:11:05 -0400

    add list_files to filesystem

--- libpsutil/system/filesystem.cpp
+++ libpsutil/system/filesystem.cpp
@@ -1,6 +1,7 @@
 #include <cell/cell_fs.h>
 
 #include "system/filesystem.hpp"
+#include "string.hpp"
 
 namespace libpsutil
 {
@@ -24,7 +25,24 @@ namespace libpsutil
             return false;
         }
 
-        bool write_file(const std::string& file_name, void* data, size_t size)
+        std::string read_file(const std::string& file_name)
+        {
+            auto size = get_file_size(file_name);
+            if (size != -1)
+            {
+                auto* data = new char[size + 1];
+                read_file(file_name, data, size);
+
+                std::string output;
+                output.resize(size + 1);
+                memcpy(const_cast<char*>(output.data()), data, static_cast<size_t>(size));
+                return output;
+            }
+
+            return {};
+        }
+
+        bool write_file(const std::string& file_name, const void* data, size_t size)
         {
             int fd = 0;
             uint64_t pos = 0, nwrite = 0;
@@ -39,6 +57,23 @@ namespace libpsutil
             }
 
             return false;
+        }
+
+        bool write_file(const std::string& file_name, const std::string& data)
+        {
+            return write_file(file_name, data.data(), data.length());
+        }
+
+        int64_t get_file_size(const std::string& file_name)
+        {
+            struct CellFsStat s;
+
+            if (cellFsStat(file_name.data(), &s) == CELL_FS_SUCCEEDED)
+            {
+                return static_cast<int64_t>(s.st_size);
+            }
+
+            return -1;
         }
 
         bool directory_exists(const std::string& directory_name)
@@ -73,16 +108,55 @@ namespace libpsutil
             }
         }
 
-        int64_t get_file_size(const std::string& file_name)
+        std::vector<std::string> list_files(const std::string& directory_name, bool recursive)
         {
-            struct CellFsStat s;
+            std::vector<std::string> files;
 
-            if (cellFsStat(file_name.data(), &s) == CELL_FS_SUCCEEDED)
+            std::string directory_name_ = directory_name;
+            if (!libpsutil::string::ends_with(directory_name, "/"))
             {
-                return static_cast<int64_t>(s.st_size);
+                directory_name_.append("/");
             }
 
-            return -1;
+            int fd;
+            if (cellFsOpendir(directory_name_.data(), &fd) != CELL_FS_OK) { return files; }
+
+            uint64_t nread;
+            CellFsDirent dent;
+
+            while (cellFsReaddir(fd, &dent, &nread) == CELL_FS_OK)
+            {
+                if (nread == 0) { break; }
+
+                if (dent.d_name[0] != '.')
+                {
+                    struct CellFsStat st;
+                    auto file = directory_name_ + dent.d_name;
+
+                    if (cellFsStat(file.data(), &st) == CELL_FS_SUCCEEDED)
+                    {
+                        if ((st.st_mode & CELL_FS_S_IFDIR) != 0)
+                        {
+                            if (recursive)
+                            {
+                                auto sub_directory = list_files(file, recursive);
+                                for (auto& sub_file : sub_directory)
+                                {
+                                    files.push_back(sub_file);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            files.push_back(file);
+                        }
+                    }
+                }
+            }
+
+            cellFsClosedir(fd);
+
+            return files;
         }
     }
 }
